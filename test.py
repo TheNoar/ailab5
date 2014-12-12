e1c458b9d07414ef85e48ae35ac6dad362ac9c96
noah dillon <noah.dillon@colorado.edu>
2014-12-11 20:57:25 -0700

    push it real good

--- test.py
+++ test.py
@@ -1,22 +1,4 @@
-import math, time, sys
-
-'''
-states = ('Healthy', 'Fever')
- 
-observations = ('normal', 'cold', 'dizzy')
- 
-start_probability = {'Healthy': 0.6, 'Fever': 0.4}
- 
-transition_probability = {
-   'Healthy' : {'Healthy': 0.7, 'Fever': 0.3},
-   'Fever' : {'Healthy': 0.4, 'Fever': 0.6}
-   }
- 
-emission_probability = {
-   'Healthy' : {'normal': 0.5, 'cold': 0.4, 'dizzy': 0.1},
-   'Fever' : {'normal': 0.1, 'cold': 0.3, 'dizzy': 0.6}
-   }
-'''   
+import getopt, math, time, sys
    
 def robotparse(fd):
     states = set()
@@ -313,7 +295,6 @@ def viterbi(obs, states, start_p, trans_p, emit_p):
  
     # Initialize base cases (t == 0)
     for y in states:
-        #V[0][y] = start_p[y] * emit_p[y][obs[0]]
         V[0][y] = (math.log10(start_p[y]) + math.log10(emit_p[y][obs[0]]))
         path[y] = [y]
     thing = len(obs)
@@ -324,20 +305,11 @@ def viterbi(obs, states, start_p, trans_p, emit_p):
         newpath = {}
  
         for y in states:
-            # lep = 0
-            # try:
-            #     lep = math.log10(emit_p[y][obs[t]])
-            # except KeyError:
-            #     err+=1
-            #     lep = math.log10(min(emit_p[y].values()))
             temp = []
             for y0 in states:
                 try:
                     temp.append(((V[t-1][y0] + math.log10(trans_p[y0][y]) + math.log10(emit_p[y][obs[t]])), y0))
                 except KeyError:
-                    # print("v",V[t-1][y0])
-                    # print("t",trans_p[y0][y])
-                    # print("e",emit_p[y][obs[t]])
 
                     temp.append((-sys.maxsize-1,y0))
             (prob, state) = max(temp)
@@ -346,14 +318,14 @@ def viterbi(obs, states, start_p, trans_p, emit_p):
  
         # Don't need to remember the old paths
         path = newpath
-        pers = int((t*100)/thing)
+        pers = int((t*100)/(thing-1)) 
         print("["+"="*int(pers/5)+" "*(20-int(pers/5))+"]: "+str(pers)+"%", end=chr(13))
     # fdout = open("trans.txt", 'w')
     # fdout.write(str(V))
     n = 0           # if only one element is observed max is sought in the initialization values
     if len(obs) != 1:
         n = t
-    #print_dptable(V)
+    # print_dptable(V)
     # print(err,thing)
     (prob, state) = max((V[n][y], y) for y in states)
     return (10**prob, path[state])
@@ -366,7 +338,14 @@ def print_dptable(V):
         s += " ".join("%.7s" % ("%f" % v[y]) for v in V)
         s += "\n"
     print(s)
-  
+
+def print_sause(table):
+    labs = sorted(max(table.values(), key=len))
+    s = "\t" + "\t".join(labs) +"\n"
+    for j in table:
+        s +=  j + "\t" + "\t".join("%.7s" %(table[j][q]) for q in labs) + "\n" 
+    return s
+
 def compare(a, b):
     if len(a)!=len(b) or len(a)==0:
         print("length error")
@@ -378,16 +357,55 @@ def compare(a, b):
     return (count*100)/len(a)
 
 
-def example():
-    # fd = open("robot_no_momemtum.data", 'r')
-    # fd = open("typos10.data", 'r')
-    fd = open("topics.data", 'r')
+def main():
+
+    try:
+        optlist, remainder = getopt.getopt(sys.argv[1:], 'p:o:h')
+        #If no arguments profided
+        if len(optlist) == 0:
+            print( "***Options required***" )
+            usage()
+        #if inappropriate argument provided
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+
+    for o, a in optlist:
+        if o == "-h":
+          usage()
+        elif o == "-p":
+            if a == "1":
+                fd = open("robot_no_momemtum.data", 'r')
+                func = robotparse
+                # states, all_observ, start_prob, trans_prob, emiss_prob, paths = robotparse(fd)
+            elif a == "2":
+                fd = open("typos10.data", 'r')
+                func = typoparse
+                # states, all_observ, start_prob, trans_prob, emiss_prob, paths = typoparse(fd)
+            elif a == "3":
+                fd = open("topics.data", 'r')
+                func = topicparse
+                # states, all_observ, start_prob, trans_prob, emiss_prob, paths = topicparse(fd)
+        elif o == "-o":
+            if a == "1":
+                states, all_observ, start_prob, trans_prob, emiss_prob, paths = func(fd)
+            elif a == "2":
+                print("Funtionality not implemented kill all humanz.")
+                sys.exit(0)
 
     fdout = open("output.txt", 'w')
+    # print(emiss_prob)
+    fdout.write(print_sause(trans_prob))
+    fdout.write(print_sause(emiss_prob))
+    # fd = open("robot_no_momemtum.data", 'r')
+    # fd = open("typos10.data", 'r')
+    # fd = open("topics.data", 'r')
+
+    
     # fdout = open("trans.txt", 'w')
     # states, all_observ, start_prob, trans_prob, emiss_prob, paths = robotparse(fd)
     # states, all_observ, start_prob, trans_prob, emiss_prob, paths = typoparse(fd)
-    states, all_observ, start_prob, trans_prob, emiss_prob, paths = topicparse(fd)
+    # states, all_observ, start_prob, trans_prob, emiss_prob, paths = topicparse(fd)
     # fdout.write(str(emiss_prob))
 
     # temp_trans = {'religion': {'religion': 0.6, 'windows': 0.08, 'cars': 0.08, 'baseball': 0.08, 'guns': 0.08, 'medicine': 0.08}, 
@@ -398,11 +416,12 @@ def example():
     #                 'medicine': {'religion': 0.07, 'windows': 0.085, 'cars': 0.085, 'medicine': 0.6, 'guns': 0.08, 'baseball': 0.08}} 
 
     # print(states)
-    print(len(all_observ[0]))
+    # print(len(all_observ[0]))
     # print(start_prob)
     # print(trans_prob)
     # print(emiss_prob)
     # return 0
+
     t1 = time.time()
     totalsame = 0
     for i in range(len(paths)):
@@ -410,23 +429,41 @@ def example():
                    tuple(states),
                    start_prob,
                    trans_prob,
-                   # trans_prob,
                    emiss_prob)
         same = compare(path, paths[i])
         totalsame += same
-        # print(prob)
         fdout.write(str(path) + "\n" + str(paths[i]) + "\n" + "Sameness: " + str(same) + "\n")
     t2 = time.time()
-    print(totalsame/len(paths))
+    
 
-     # return viterbi(tuple(observ),
-     #               tuple(states),
-     #               start_prob,
-     #               trans_prob,
-     #               emiss_prob)
+    print("The output is in the output.txt file")
+
+    print("Accuracy: " +str(totalsame/len(paths)))
+
     timeness = t2 - t1
     ret = str(int((timeness/60)/60))+":"+str(int((timeness/60)%60))+":"+str(int(timeness%60))
+    
+
+
+
     return ret
     
     #return viterbi(robotparse(fd))
-print(example())
+
+
+def usage():
+  print( """
+  Usage:
+  ---------
+    Flags
+    -p: Choose the file to Run (1 for robot, 2 for typo correct, and 3 for topic change)
+    -o: Specify the HMM order (1 or 2)
+
+    Example Usage: 
+    "python assignment5.py -p 1 -o 1" will run the toy robot with a first order HMM
+  ---------
+  """ )
+  sys.exit(2)
+
+if __name__ == "__main__":
+    print(main())
